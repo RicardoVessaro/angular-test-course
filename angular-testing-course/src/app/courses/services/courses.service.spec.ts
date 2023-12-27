6a14cf3604fe24e685e2f6345c035f274e345132
RicardoVessaro <ricardolvessaro@gmail.com>
2023-12-27 20:06:17 -0300

    S2 #12 Testing Angular HTTP Services - Test Setup with the HttpClientTestingModule

---
+++ angular-testing-course/src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
+
+import { CoursesService } from "./courses.service";
+
+describe("CoursesService", () => {
+
+    let coursesService: CoursesService;
+    let httpTestingController: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                CoursesService
+            ]
+        })
+
+        coursesService = TestBed.inject(CoursesService);
+        httpTestingController = TestBed.inject(HttpTestingController);
+    });
+
+    it('should retrieve all courses', () => {
+
+    });
+
+});
