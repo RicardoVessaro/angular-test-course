ecb5d3645457ecd3131d70bf16a5f41fcfaeab2e
RicardoVessaro <ricardolvessaro@gmail.com>
2023-12-26 21:50:06 -0300

    S1 #5 Quick Introduction to Jasmine - Test Suites and Specifications

---
+++ angular-testing-course/src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,17 @@
+
+describe('CalculatorService', () => {   // Declare the test suit
+
+    it('should add two numbers', () => {
+
+        pending();
+
+    });   // Declare the test instruction
+
+
+    it('should subtract two numbers', () => {
+
+        fail();
+
+    });  
+
+});
