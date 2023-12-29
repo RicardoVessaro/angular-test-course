e74ff66dcad320de42df54eba6234f6ae462c954
RicardoVessaro <ricardolvessaro@gmail.com>
2023-12-29 19:59:46 -0300

    S4 #37 Understanding the Angular waitForAsync()  Test Zone

--- angular-testing-course/src/app/courses/home/home.component.spec.ts
+++ angular-testing-course/src/app/courses/home/home.component.spec.ts
@@ -96,7 +96,7 @@ describe('HomeComponent', () => {
   });
 
 
-  it("should display advanced courses when tab clicked", fakeAsync(() => { 
+  it("should display advanced courses when tab clicked - fakeAsync", fakeAsync(() => { 
 
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));
 
@@ -115,6 +115,30 @@ describe('HomeComponent', () => {
     expect(cardTitles.length).toBeGreaterThan(0, "Could not find card titles");
 
     expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
+  }));
+
+  it("should display advanced courses when tab clicked - waitForAsync", waitForAsync(() => { 
+
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mdc-tab'));
+
+    click(tabs[1]);
+
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      console.log('called whenStable()');
+
+      const cardTitles = el.queryAll(By.css('.mat-mdc-tab-body-active .mat-mdc-card-title'));
+    
+      expect(cardTitles.length).toBeGreaterThan(0, "Could not find card titles");
+  
+      expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
+    });
+
   }));
 
 });
