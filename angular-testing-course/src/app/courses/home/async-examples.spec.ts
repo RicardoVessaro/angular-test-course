deea6b6fc968bfb7d2c13f62193934cb2f431c38
RicardoVessaro <ricardolvessaro@gmail.com>
2023-12-28 21:57:28 -0300

    S4 #33 Testing Asynchronoous Code with fakeAsync - Full Example

--- angular-testing-course/src/app/courses/home/async-examples.spec.ts
+++ angular-testing-course/src/app/courses/home/async-examples.spec.ts
@@ -43,7 +43,7 @@ fdescribe("Async Testing Examples", () => {
         expect(test).toBeTruthy();
     }));
 
-    fit('Asyncronous test example - plain Promise', fakeAsync(() => {
+    it('Asyncronous test example - plain Promise', fakeAsync(() => {
 
         let test = false;
 
@@ -81,4 +81,36 @@ fdescribe("Async Testing Examples", () => {
         expect(test).toBeTruthy();
 
     }));
+
+    fit('Aynchronous test example - Promises + setTimeout()', fakeAsync(() => {
+
+        let counter = 0;
+
+        Promise.resolve()
+            .then(() => {
+
+                counter += 10;
+
+                setTimeout(() => {
+
+                    counter += 1;
+
+                }, 1000);
+            });
+
+        expect(counter).toBe(0);
+
+        flushMicrotasks();
+
+        expect(counter).toBe(10);
+
+        tick(500);
+
+        expect(counter).toBe(10);
+
+        tick(500);
+
+        expect(counter).toBe(11);
+    }));
+
 });
