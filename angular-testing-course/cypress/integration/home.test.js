63ecb02f94e9a4e7c934baf44028947203e92c23
RicardoVessaro <ricardolvessaro@gmail.com>
2023-12-29 21:06:48 -0300

    S4 #40 How to Mock an HTTP Backend using Cypress

--- angular-testing-course/cypress/integration/home.test.js
+++ angular-testing-course/cypress/integration/home.test.js
@@ -3,8 +3,19 @@ describe('Home Page', () => {
 
     it('should display a list of courses', () => {
 
+        cy.fixture('courses.json').as('coursesJSON');
+
+        cy.server();
+
+        cy.route('/api/courses', '@coursesJSON').as('courses');
+
         cy.visit('/'); // Access the application with the configured base URL 
+
         cy.contains("All Courses");
+
+        cy.wait('@courses');
+
+        cy.get('mat-card').should("have.length", 9);
 
     });
 });
